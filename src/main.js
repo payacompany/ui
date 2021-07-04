879db96dbefce486a516a1d2da948488e27e8c1d
nimatorabi <nimatorabi@rocketmail.com>
2021-07-04 13:15:29 +0430

    install vuesax uikit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import "./assets/styles/css/index.css";
 import { ValidationProvider } from "vee-validate";
 Vue.component("ValidationProvider", ValidationProvider);
 
+import Vuesax from "vuesax";
+
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+Vue.use(Vuesax, {
+	// eslint-disable-next-line prettier-vue/prettier
+	rtl: true,
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
