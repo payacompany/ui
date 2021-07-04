9b2d905ed66f9b54ee2cf593f1c8608073f15b32
nimatorabi <nimatorabi@rocketmail.com>
2021-07-04 13:17:24 +0430

    install vue sweetalert2

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ Vue.use(Vuesax, {
 	rtl: true,
 });
 
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+Vue.use(VueSweetalert2);
+
 Vue.config.productionTip = false;
 
 new Vue({
