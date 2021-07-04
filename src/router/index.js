e94737172998318edd5fc52af35e8e9c899f5ff8
nimatorabi <nimatorabi@rocketmail.com>
2021-07-04 12:13:12 +0430

    install and add vue-router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+router.afterEach(() => {});
+
+router.beforeEach((to, from, next) => {});
+export default router;
