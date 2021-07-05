cc9e377fc70577b2ffef7f1fa2c67dd3ef312416
nimatorabi <nimatorabi@rocketmail.com>
2021-07-05 10:25:56 +0430

    complete verification step component and add main color to tailwind colors

--- src/components/pages/dashboard/profile/userVerification/VerificationStep.vue
+++ src/components/pages/dashboard/profile/userVerification/VerificationStep.vue
@@ -1,8 +1,20 @@
 <template>
-	<div class="py-3">
-		<p>
+	<div class="py-3 flex justify-between items-center">
+		<p class="text-bold" :class="[step.isCompleted ? 'text-gray-400' : '']">
+			<span> {{ index + 1 }}. </span>
 			{{ step.name }}
 		</p>
+		<div>
+			<vs-button v-if="!step.isCompleted" size="small">
+				اعتبارسنجی
+			</vs-button>
+			<p v-else class="text-xs text-success">
+				اطلاعات تایید شده
+				<span class="material-icons">
+					check
+				</span>
+			</p>
+		</div>
 	</div>
 </template>
 
@@ -21,4 +33,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.material-icons {
+	font-size: 16px;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#5b3cc4",
+				success: "rgb(23, 201, 100)",
+				danger: "rgb(242, 19, 93)",
+				warning: "rgb(255, 130, 0)",
+				dark: "rgb(36, 33, 69)",
+			},
+		},
 	},
 	variants: {
 		extend: {},
