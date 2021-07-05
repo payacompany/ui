b774ced7ee44dd6bd21be3f16f3e1e1cc193d8da
nimatorabi <nimatorabi@rocketmail.com>
2021-07-05 16:50:08 +0430

    add generator for qrcode to project

--- src/views/security/2fa.vue
+++ src/views/security/2fa.vue
@@ -1,11 +1,34 @@
 <template>
-	<div>
-		sec
+	<div class="shadow-xl w-6/12 center rounded-md py-8">
+		<p class="text-bold text-xl text-center px-8">
+			شناسایی دوعاملی
+		</p>
+		<vs-divider />
+		<div class="px-8 mt-5">
+			<qr-code text="Text to encode" />
+			<div class="mt-5">
+				<vs-button class="w-full">فعالسازی</vs-button>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import VueQRCodeComponent from "vue-qrcode-component";
+export default {
+	components: {
+		"qr-code": VueQRCodeComponent
+	},
+	data () {
+		return {};
+	}
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+::v-deep {
+	.con-input-upload {
+		margin: 0;
+	}
+}
+</style>
