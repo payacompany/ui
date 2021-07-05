c98d21c094f30148ba5bef76c25db33e802886e3
nimatorabi <nimatorabi@rocketmail.com>
2021-07-05 10:12:06 +0430

    create verification step component

---
+++ src/components/pages/dashboard/profile/userVerification/VerificationStep.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="py-3">
+		<p>
+			{{ step.name }}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		index: {
+			type: String,
+			default: "1"
+		},
+		step: {
+			type: Object,
+			default: () => {}
+		}
+	}
+};
+</script>
+
+<style></style>
--- src/components/pages/dashboard/profile/userVerification/index.vue
+++ src/components/pages/dashboard/profile/userVerification/index.vue
@@ -1,11 +1,59 @@
 <template>
 	<div>
-		kyc
+		<p class="text-base font-bold">احراز هویت</p>
+		<div class="flex flex-row justify-between">
+			<div v-for="(step, index) in kycSteps" :key="index" class="mt-2">
+				<vs-button
+					size="small"
+					class="w-full font-bold"
+					:color="step.isCompleted ? 'success' : 'primary'"
+				>
+					{{ step.title }}
+				</vs-button>
+			</div>
+		</div>
+		<div v-for="(step, index) in kycSteps" :key="index" class="mt-2">
+			<verification-step :step="step" :index="index" />
+		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import VerificationStep from "./VerificationStep.vue";
+export default {
+	components: { VerificationStep },
+	data () {
+		return {
+			kycSteps: [
+				{
+					title: "مرحله اول",
+					name: "احراز ایمیل",
+					isCompleted: true
+				},
+				{
+					title: "مرحله دوم",
+					name: "اعتبارسنجی شماره موبایل",
+					isCompleted: true
+				},
+				{
+					title: "مرحله سوم",
+					name: "تکمیل اطلاعات کاربری",
+					isCompleted: false
+				},
+				{
+					title: "مرحله چهارم",
+					name: "ارسال مدارک",
+					isCompleted: false
+				},
+				{
+					title: "مرحله پنجم",
+					name: "اطلاعات محل سکونت",
+					isCompleted: false
+				}
+			]
+		};
+	}
+};
 </script>
 
 <style></style>
