28979bbb2a7d36f5b1be64ab99781ae2d2fc6a27
nimatorabi <nimatorabi@rocketmail.com>
2021-07-05 15:17:15 +0430

    userBankAccounts completed

--- src/views/dashboard/profile/bankAccounts/index.vue
+++ src/views/dashboard/profile/bankAccounts/index.vue
@@ -1,11 +1,46 @@
 <template>
-	<div>
-		bank accounts
+	<div class="shadow-xl w-3/12 center rounded-md py-8">
+		<p class="text-bold text-xl text-center px-8">
+			اطلاعات حساب بانکی
+		</p>
+		<vs-divider />
+		<div class="px-8 mt-2">
+			<div class="flex flex-col justify-between">
+				<vs-input
+					v-model="bankAccounts.shabaNum"
+					label-placeholder="شماره شبا"
+				/>
+				<div class="mt-2">
+					<vs-input
+						v-model="bankAccounts.shabaNum"
+						label-placeholder="شماره کارت"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="mt-5 px-8">
+			<vs-button class="w-full">ثبت اطلاعات بانکی</vs-button>
+		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data () {
+		return {
+			bankAccounts: {
+				shabaNum: null,
+				bankNumber: null
+			}
+		};
+	}
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+::v-deep {
+	.vs-input--input.normal {
+		width: 21.5vw;
+	}
+}
+</style>
