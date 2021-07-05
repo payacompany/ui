3f4488679d73dfe7c1ba71ffe7d85be9c230c3f3
nimatorabi <nimatorabi@rocketmail.com>
2021-07-05 17:25:29 +0430

    change styles in table

--- src/components/pages/dashboard/accountActivity/accountActivityTable.vue
+++ src/components/pages/dashboard/accountActivity/accountActivityTable.vue
@@ -51,3 +51,99 @@ export default {
 	})
 };
 </script>
+
+<style lang="scss" scoped>
+::v-deep {
+	.vs-con-table {
+		.product-name {
+			max-width: 23rem;
+		}
+
+		.vs-table--header {
+			display: flex;
+			flex-wrap: wrap-reverse;
+			margin-left: 1.5rem;
+			margin-right: 1.5rem;
+
+			> span {
+				display: flex;
+				flex-grow: 1;
+			}
+
+			.vs-table--search {
+				padding-top: 0;
+
+				.vs-table--search-input {
+					padding: 0.9rem 2.5rem;
+					font-size: 1rem;
+
+					& + i {
+						right: 1rem;
+						left: auto;
+						transform: rotateY(180deg);
+					}
+
+					&:focus + i {
+						right: 1rem;
+						left: auto;
+						transform: rotateY(0deg);
+					}
+				}
+			}
+		}
+
+		.vs-table {
+			border-collapse: separate;
+			border-spacing: 0 1rem;
+			padding: 0 1rem;
+
+			tr {
+				box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
+
+				td {
+					padding: 20px;
+
+					&:first-child {
+						border-top-left-radius: 0.5rem;
+						border-bottom-left-radius: 0.5rem;
+					}
+
+					&:last-child {
+						border-top-right-radius: 0.5rem;
+						border-bottom-right-radius: 0.5rem;
+					}
+				}
+
+				td.td-check {
+					padding: 20px !important;
+				}
+			}
+		}
+
+		.vs-table--thead {
+			th {
+				padding-top: 0;
+				padding-bottom: 0;
+
+				.vs-table-text {
+					text-transform: uppercase;
+					font-weight: 600;
+				}
+			}
+
+			th.td-check {
+				padding: 0 15px !important;
+			}
+
+			tr {
+				background: none;
+				box-shadow: none;
+			}
+		}
+
+		.vs-table--pagination {
+			justify-content: center;
+		}
+	}
+}
+</style>
