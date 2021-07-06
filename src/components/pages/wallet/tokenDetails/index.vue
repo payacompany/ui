e2af8970d6c49f1ab78560f8c5914587be65146a
nimatorabi <nimatorabi@rocketmail.com>
2021-07-06 09:51:25 +0430

    add vs-tab for tokenDetails component

--- src/components/pages/wallet/tokenDetails/index.vue
+++ src/components/pages/wallet/tokenDetails/index.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		token details
+		<vs-tabs alignment="left">
+			<vs-tab label="واریز">
+				<div>
+					واریز
+				</div>
+			</vs-tab>
+			<vs-tab label="برداشت">
+				<div>
+					برداشت
+				</div>
+			</vs-tab>
+		</vs-tabs>
 	</div>
 </template>
 
