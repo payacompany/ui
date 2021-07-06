2fba0011eac1121e39da23edbef6044a0623d6d2
nimatorabi <nimatorabi@rocketmail.com>
2021-07-06 12:49:15 +0430

    start to create transactions tab components

--- src/components/pages/wallet/tokenDetails/index.vue
+++ src/components/pages/wallet/tokenDetails/index.vue
@@ -7,17 +7,22 @@
 			<vs-tab label="برداشت">
 				<withdraw :coins="coins" />
 			</vs-tab>
+			<vs-tab label="تاریخچه">
+				<transactions />
+			</vs-tab>
 		</vs-tabs>
 	</div>
 </template>
 
 <script>
 import Deposit from "./Deposit.vue";
+import Transactions from "./Transactions.vue";
 import Withdraw from "./Withdraw.vue";
 export default {
 	components: {
 		Withdraw,
-		Deposit
+		Deposit,
+		Transactions
 	},
 	props: {
 		coins: {
