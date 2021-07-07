fcfe5623619ec49178aa63db30aa454dff0c5b99
nimatorabi <nimatorabi@rocketmail.com>
2021-07-07 12:45:55 +0430

    create buy-order-tab component

---
+++ src/components/pages/trading/BuyPlaceOrder.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="w-11/12 mx-auto">
+		<vs-select v-model="selectedType" label="نوع سفارش" class="min-w-full">
+			<vs-select-item
+				v-for="(item, index) in types"
+				:key="index"
+				:value="item.value"
+				:text="item.text"
+			/>
+		</vs-select>
+		<vs-input
+			class="min-w-full"
+			label="قیمت"
+			placeholder="قیمت مورد نظر را وارد نمایید"
+		/>
+		<vs-input
+			class="min-w-full"
+			label="حجم"
+			placeholder="حجم مورد نظر را وارد نمایید"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			selectedType: 0,
+			types: [
+				{ text: "محدود", value: 0 },
+				{ text: "بازار", value: 1 }
+			]
+		};
+	}
+};
+</script>
+
+<style></style>
--- src/components/pages/trading/PlaceOrder.vue
+++ src/components/pages/trading/PlaceOrder.vue
@@ -1,14 +1,19 @@
 <template>
 	<div class="shadow-md rounded-md">
 		<vs-tabs alignment="center">
-			<vs-tab label="خرید"> test </vs-tab>
+			<vs-tab label="خرید">
+				<buy-place-order />
+			</vs-tab>
 			<vs-tab label="فروش"> test </vs-tab>
 		</vs-tabs>
 	</div>
 </template>
 
 <script>
-export default {};
+import BuyPlaceOrder from "./BuyPlaceOrder.vue";
+export default {
+	components: { BuyPlaceOrder }
+};
 </script>
 
 <style lang="scss" scoped>
