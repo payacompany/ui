37b2b7e4e8cb0838f01540fc83c2fd34e5ba57dd
nimatorabi <nimatorabi@rocketmail.com>
2021-07-07 14:32:18 +0430

    market component

--- src/components/pages/trading/markets/MarketList.vue
+++ src/components/pages/trading/markets/MarketList.vue
@@ -1,11 +1,82 @@
 <template>
 	<div>
-		market list
+		<vs-table :data="markets" strip no-data-text="رکوردی موجود نیست">
+			<template slot="thead">
+				<vs-th>بازار</vs-th>
+				<vs-th>قیمت</vs-th>
+				<vs-th>درصد تغییرات</vs-th>
+			</template>
+
+			<template slot-scope="{ data }">
+				<vs-tr
+					v-for="(tr, indextr) in data"
+					:key="indextr"
+					class="cursor-pointer"
+				>
+					<vs-td :data="data[indextr].name">
+						{{ data[indextr].name }}
+					</vs-td>
+
+					<vs-td :data="data[indextr].price">
+						{{ data[indextr].price }}
+					</vs-td>
+
+					<vs-td
+						:data="data[indextr].changes"
+						:class="[
+							data[indextr].changes >= 0 ? 'text-success' : 'text-danger'
+						]"
+					>
+						{{ data[indextr].changes }}%
+					</vs-td>
+				</vs-tr>
+			</template>
+		</vs-table>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data () {
+		return {
+			markets: [
+				{ name: "BTC/USDT", price: 30000, changes: 0 },
+				{ name: "ETH/USDT", price: 1200, changes: -5 }
+			]
+		};
+	}
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+::v-deep {
+	.vs-table {
+		.vs-table--thead {
+			background: transparent !important;
+			th {
+				.vs-table-text {
+					color: black;
+					text-transform: uppercase;
+					font-weight: normal;
+					display: flex;
+					justify-content: center;
+				}
+			}
+			th.td-check {
+				padding: 0 15px !important;
+			}
+			tr {
+				background: none;
+				box-shadow: none;
+			}
+		}
+	}
+	.vs-table--tbody {
+		overflow: hidden;
+		text-align: center;
+	}
+	.vs-table--tbody-table {
+		min-width: auto;
+	}
+}
+</style>
--- src/components/pages/trading/markets/index.vue
+++ src/components/pages/trading/markets/index.vue
@@ -1,16 +1,36 @@
 <template>
-	<div class="shadow-md rounded-md mt-2 py-2">
+	<div class="shadow-md rounded-md mt-2 py-1">
 		<div class="px-5 pt-2">
 			<p class="font-bold text-sm">
 				بازار ها
 			</p>
 		</div>
 		<vs-divider />
+		<market-list />
+		<vs-input
+			v-model="search"
+			class="min-w-full px-3"
+			icon="search"
+			placeholder="جستجو بازار مورد نظر"
+		/>
 	</div>
 </template>
 
 <script>
-export default {};
+import MarketList from "./MarketList.vue";
+
+export default {
+	components: { MarketList },
+	data () {
+		return {
+			search: ""
+		};
+	}
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.vs-divider {
+	margin: 10px 0;
+}
+</style>
