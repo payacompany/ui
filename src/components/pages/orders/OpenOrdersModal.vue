27dfea483f06c1afbf69306495dee1f189f6e794
nimatorabi <nimatorabi@rocketmail.com>
2021-07-07 09:56:42 +0430

    create modal for orderInfo

---
+++ src/components/pages/orders/OpenOrdersModal.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+	<div class="centerx">
+		<vs-button color="primary" type="filled" @click="showModal = true">
+			اطلاعات بیشتر
+		</vs-button>
+		<vs-popup title="جزئیات سفارش" :active.sync="showModal">
+			<div class="flex flex-wrap justify-between">
+				<p>
+					شناسه سفارش:
+					<span>
+						{{ orderDetails.id }}
+					</span>
+				</p>
+				<p>
+					تاریخ سفارش:
+					<span> {{ orderDetails.date }} </span>
+				</p>
+				<p>
+					قیمت واحد سفارش:
+					<span> {{ orderDetails.priceInUnit }} تومان </span>
+				</p>
+				<p>
+					حجم سفارش:
+					<span> {{ orderDetails.volume }} </span>
+				</p>
+				<p>
+					میزان پر شده:
+					<span> {{ orderDetails.volume }} </span>
+				</p>
+			</div>
+		</vs-popup>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		orderDetails: {
+			type: Object,
+			default: () => {}
+		}
+	},
+	data () {
+		return {
+			showModal: false
+		};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+::v-deep {
+	.vs-button {
+		border-radius: 25px;
+	}
+	.vs-button--text {
+		color: white !important;
+	}
+}
+</style>
--- src/components/pages/orders/index.vue
+++ src/components/pages/orders/index.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="w-8/12 mx-auto shadow-lg rounded-md">
 		<vs-tabs alignment="left">
-			<vs-tab label="سفارشات کل" icon="add">
+			<vs-tab label="سفارشات کل">
 				سفارشات
 			</vs-tab>
-			<vs-tab label="سفارشات باز" icon="payments">
-				سفارشات
+			<vs-tab label="سفارشات باز">
+				<open-orders />
 			</vs-tab>
-			<vs-tab label="سفارشات تکمیل شده" icon="history">
+			<vs-tab label="سفارشات تکمیل شده">
 				سفارشات
 			</vs-tab>
 		</vs-tabs>
@@ -15,7 +15,10 @@
 </template>
 
 <script>
-export default {};
+import OpenOrders from "./OpenOrders.vue";
+export default {
+	components: { OpenOrders }
+};
 </script>
 
 <style></style>
