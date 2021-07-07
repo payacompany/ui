4d3e64c3dea0576cd6c4302c32a1fc5886ffb5ff
nimatorabi <nimatorabi@rocketmail.com>
2021-07-07 11:15:19 +0430

    create historty tab component page

---
+++ src/components/pages/history/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="w-8/12 mx-auto shadow-lg rounded-md">
+		<vs-tabs alignment="left">
+			<vs-tab label="تاریخچه واریز">
+				تاریخچه
+			</vs-tab>
+			<vs-tab label="تاریخچه برداشت">
+				تاریخچه
+			</vs-tab>
+			<vs-tab label="تاریخچه معاملات">
+				تاریخچه
+			</vs-tab>
+		</vs-tabs>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/history/index.vue
+++ src/views/history/index.vue
@@ -1,11 +1,16 @@
 <template>
-	<div>
-		history
+	<div class="container mx-auto mt-12">
+		<history />
 	</div>
 </template>
 
 <script>
-export default {};
+import History from "../../components/pages/history/index.vue";
+export default {
+	components: {
+		History
+	}
+};
 </script>
 
 <style></style>
