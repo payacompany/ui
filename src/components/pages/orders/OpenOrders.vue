34276c39e33b300eab0638218dc8d7ca1a5ed59d
nimatorabi <nimatorabi@rocketmail.com>
2021-07-07 09:10:15 +0430

    openOrders component

---
+++ src/components/pages/orders/OpenOrders.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		سفارشات باز
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/components/pages/orders/index.vue
+++ src/components/pages/orders/index.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="w-8/12 mx-auto shadow-lg rounded-md">
-		orders
+		<vs-tabs alignment="left">
+			<vs-tab label="سفارشات کل" icon="add">
+				سفارشات
+			</vs-tab>
+			<vs-tab label="سفارشات باز" icon="payments">
+				سفارشات
+			</vs-tab>
+			<vs-tab label="سفارشات تکمیل شده" icon="history">
+				سفارشات
+			</vs-tab>
+		</vs-tabs>
 	</div>
 </template>
 
