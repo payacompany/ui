c86ab64837c351c33d8c1cc6881dad405b3ada71
nimatorabi <nimatorabi@rocketmail.com>
2021-07-08 10:24:33 +0430

    create languageswitcher component

---
+++ src/components/global/header/LanguageSwitcher.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="locale-switcher">🌐 {{ $i18n.locale }}</div>
+</template>
+<script>
+import i18n from "@/i18n";
+
+export default {
+	methods: {
+		setLocale (locale) {
+			this.$i18n.locale = locale;
+		}
+	},
+	mounted () {
+		console.log("Active locale: ", i18n.locale);
+	}
+};
+</script>
--- src/components/global/header/index.vue
+++ src/components/global/header/index.vue
@@ -1,17 +1,19 @@
 <template>
 	<div class="shadow-lg">
 		<div class="container-fluid mx-4 py-4 flex flex-row justify-between">
-			<night-mode />
+			<!-- <night-mode /> -->
+			<language-switcher />
 			<side-bar />
 		</div>
 	</div>
 </template>
 
 <script>
+import LanguageSwitcher from "./LanguageSwitcher.vue";
 import NightMode from "./NightMode.vue";
 import SideBar from "./SideBar.vue";
 export default {
-	components: { NightMode, SideBar }
+	components: { NightMode, SideBar, LanguageSwitcher }
 };
 </script>
 
