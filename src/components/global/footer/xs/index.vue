47aaae3e85fb251191bf7fae474742663217fd29
nimatorabi <nimatorabi@rocketmail.com>
2021-07-11 09:36:37 +0430

    footer xs completed

--- src/components/global/footer/xs/index.vue
+++ src/components/global/footer/xs/index.vue
@@ -1,25 +1,37 @@
 <template>
-	<div class="footer block md:hidden shadow-md border-t-2 border-gray-400">
-		<nav class="flex flex-row items-baseline pt-1 w-full">
+	<div class="footer block md:hidden shadow-md border-t border-gray-300">
+		<nav class="flex flex-row items-baseline pt-2 w-full">
 			<ul class="w-full flex flex-row justify-between px-3 text-xs">
-				<li class="flex flex-col justify-center text-center">
+				<li class="flex flex-col justify-center text-center text-gray-500">
 					<span class="material-icons">
 						account_circle
 					</span>
 
 					{{ $t("nav.profile") }}
 				</li>
-				<li>
+				<li class="flex flex-col justify-center text-center text-gray-500">
+					<span class="material-icons">
+						account_balance_wallet
+					</span>
 					{{ $t("nav.trade") }}
 				</li>
 
-				<li>
+				<li class="flex flex-col justify-center text-center text-gray-500">
+					<span class="material-icons">
+						signal_cellular_alt
+					</span>
 					{{ $t("nav.wallet") }}
 				</li>
-				<li>
+				<li class="flex flex-col justify-center text-center text-gray-500">
+					<span class="material-icons">
+						dvr
+					</span>
 					{{ $t("nav.orders") }}
 				</li>
-				<li>
+				<li class="flex flex-col justify-center text-center text-gray-500">
+					<span class="material-icons">
+						history
+					</span>
 					{{ $t("nav.history") }}
 				</li>
 			</ul>
@@ -37,5 +49,8 @@ export default {};
 	width: 100%;
 	bottom: 0;
 	height: 54px;
+}
+span {
+	font-size: 18px;
 }
 </style>
