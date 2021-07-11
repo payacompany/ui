3c13e4f0af35f635c04f29fdc1a5c64718eb840f
nimatorabi <nimatorabi@rocketmail.com>
2021-07-11 09:27:50 +0430

    add routes to footer xs

--- src/components/global/footer/xs/index.vue
+++ src/components/global/footer/xs/index.vue
@@ -1,6 +1,29 @@
 <template>
-	<div class="footer shadow-md">
-		footer xs
+	<div class="footer block md:hidden shadow-md border-t-2 border-gray-400">
+		<nav class="flex flex-row items-baseline pt-1 w-full">
+			<ul class="w-full flex flex-row justify-between px-3 text-xs">
+				<li class="flex flex-col justify-center text-center">
+					<span class="material-icons">
+						account_circle
+					</span>
+
+					{{ $t("nav.profile") }}
+				</li>
+				<li>
+					{{ $t("nav.trade") }}
+				</li>
+
+				<li>
+					{{ $t("nav.wallet") }}
+				</li>
+				<li>
+					{{ $t("nav.orders") }}
+				</li>
+				<li>
+					{{ $t("nav.history") }}
+				</li>
+			</ul>
+		</nav>
 	</div>
 </template>
 
@@ -10,7 +33,9 @@ export default {};
 
 <style lang="scss" scoped>
 .footer {
-	position: relative;
+	position: fixed;
+	width: 100%;
 	bottom: 0;
+	height: 54px;
 }
 </style>
