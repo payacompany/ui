272e63c30e8035c9505b5c32f04ca1ba87fb6b01
nimatorabi <nimatorabi@rocketmail.com>
2021-07-13 11:16:39 +0430

    create structure for layouts

---
+++ src/layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		error layout
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/layouts/trade.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		trade layout
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
