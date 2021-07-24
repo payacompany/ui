d69070012e2591deff4cde743546f9b02221ec2a
nimatorabi <nimatorabi@rocketmail.com>
2021-07-24 09:56:39 +0430

    change style in wallet

--- src/components/pages/wallet/tokenDetails/index.vue
+++ src/components/pages/wallet/tokenDetails/index.vue
@@ -1,13 +1,25 @@
 <template>
 	<div>
-		<vs-tabs alignment="left">
-			<vs-tab :label="$t('pages.wallet.deposit.title')" icon="add">
+		<vs-tabs alignment="left" :color="borderColor">
+			<vs-tab
+				:label="$t('pages.wallet.deposit.title')"
+				icon="add"
+				@click="borderColor = 'success'"
+			>
 				<deposit />
 			</vs-tab>
-			<vs-tab :label="$t('pages.wallet.withdraw.title')" icon="minimize">
+			<vs-tab
+				:label="$t('pages.wallet.withdraw.title')"
+				icon="minimize"
+				@click="borderColor = 'danger'"
+			>
 				<withdraw :coins="coins" />
 			</vs-tab>
-			<vs-tab :label="$t('pages.wallet.history.title')" icon="history">
+			<vs-tab
+				:label="$t('pages.wallet.history.title')"
+				icon="history"
+				@click="borderColor = 'primary'"
+			>
 				<transactions />
 			</vs-tab>
 		</vs-tabs>
@@ -30,20 +42,18 @@ export default {
 			default: () => [],
 		},
 	},
+	data() {
+		return {
+			borderColor: "success",
+		};
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 ::v-deep {
-	// .vs-tabs--li {
-	// 	.vs-tabs--btn {
-	// 		align-items: center !important;
-	// 	}
-	// 	&:nth-child(2) {
-	// 		.vs-tabs--btn {
-	// 			align-items: baseline !important;
-	// 		}
-	// 	}
-	// }
+	.material-icons {
+		color: inherit;
+	}
 }
 </style>
