662c87d18753c0f50a5133ffa9dea653461675a8
nimatorabi <nimatorabi@rocketmail.com>
2021-08-05 12:42:28 +0430

    add log for test

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,7 @@ export default {
 	userInformations({ commit }, payload) {
 		try {
 			const { data } = axios.get(`/barong/resource/users/me`, payload);
+			console.log(data);
 			commit("SET_MY_PROFILE", data);
 		} catch {}
 	},
