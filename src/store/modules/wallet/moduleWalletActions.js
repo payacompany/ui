e5a227d6615737b74924ecf8f40f2bc688c9588b
nimatorabi <nimatorabi@rocketmail.com>
2021-08-08 09:14:05 +0430

    change method request

--- src/store/modules/wallet/moduleWalletActions.js
+++ src/store/modules/wallet/moduleWalletActions.js
@@ -3,7 +3,7 @@ import axios from "../../../axios";
 // import axios from "@/axios.js";
 export default {
 	getBalances({ commit }) {
-		axios.post("/peatio/account/balances").then(res => {
+		axios.get("/peatio/account/balances").then(res => {
 			commit("SET_WALLET_BALANCES", res.data);
 		});
 	},
