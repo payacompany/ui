8672abf4d52c347954b94ab536b82776fe89397d
nimatorabi <nimatorabi@rocketmail.com>
2021-08-09 14:20:57 +0430

    add withdraw rules

--- src/store/modules/wallet/moduleWalletActions.js
+++ src/store/modules/wallet/moduleWalletActions.js
@@ -2,13 +2,13 @@ import axios from "../../../axios";
 
 // import axios from "@/axios.js";
 export default {
-	getBalances({ commit }) {
-		axios.get("/peatio/account/balances").then(res => {
+	async getBalances({ commit }) {
+		await axios.get("/peatio/account/balances").then(res => {
 			commit("SET_WALLET_BALANCES", res.data);
 		});
 	},
-	getCurrencies({ commit }) {
-		axios.get("/peatio/public/currencies").then(res => {
+	async getCurrencies({ commit }) {
+		await axios.get("/peatio/public/currencies").then(res => {
 			commit("SET_CURRENCIES", res.data);
 		});
 	},
