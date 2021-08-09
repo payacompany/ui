8019c4fe3b458299419656dea5b4e93f0154a94f
nimatorabi <nimatorabi@rocketmail.com>
2021-08-09 14:55:42 +0430

    change response log

--- src/store/modules/wallet/moduleWalletActions.js
+++ src/store/modules/wallet/moduleWalletActions.js
@@ -2,20 +2,13 @@ import axios from "../../../axios";
 
 // import axios from "@/axios.js";
 export default {
-	getMyWalletInformation({ commit }) {
-		axios
-			.all([
-				axios.get("/peatio/account/balances"),
-				axios.get("/peatio/public/currencies"),
-				axios.get("/peatio/account/transactions"),
-			])
-			.then(res => {
-				console.log("response", res);
-			})
-			.catch(errors => {
-				// react on errors.
-				console.error(errors);
+	async getMyWalletInformation({ commit }) {
+		await axios.get("/peatio/account/balances").then(res => {
+			console.log("balances", res);
+			axios.get("/peatio/public/currencies").then(res => {
+				console.log("currencies", res);
 			});
+		});
 	},
 	// async getBalances({ commit }) {
 	// 	await axios.get("/peatio/account/balances").then(res => {
