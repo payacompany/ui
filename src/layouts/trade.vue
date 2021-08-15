029a3d929365a0d493a5fec57e175f811ebb515c
nimatorabi <nimatorabi@rocketmail.com>
2021-08-15 10:36:59 +0430

    connect socket

--- src/layouts/trade.vue
+++ src/layouts/trade.vue
@@ -15,6 +15,16 @@ export default {
 	components: {
 		HeaderBar,
 		HeaderXs
+	},
+	sockets: {
+		connect: function () {
+			console.log("socket connected");
+		},
+		customEmit: function (data) {
+			console.log(
+				'this method was fired by the socket server. eg: io.emit("customEmit", data)'
+			);
+		}
 	}
 };
 </script>
