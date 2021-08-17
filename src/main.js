fdb193f31ed7cb617a1b3bcf47950c2fc71aa2b2
nimatorabi <nimatorabi@rocketmail.com>
2021-08-17 13:48:50 +0430

    restore

--- src/main.js
+++ src/main.js
@@ -64,8 +64,20 @@ Vue.component("error", Error);
 Vue.component("trade", Trade);
 Vue.component("xs", Xs);
 
-import VueSocketIO from "vue-socket.io";
-Vue.use(VueSocketIO);
+// import VueSocketIO from "vue-socket.io";
+// Vue.use(
+// 	new VueSocketIO({
+// 		debug: true,
+// 		secure: true,
+// 		connection:
+// 			"ws://dax.autovm.ir/api/v2/ranger/public/?stream=global.tickers",
+// 		vuex: {
+// 			store,
+// 			actionPrefix: "SOCKET_",
+// 			mutationPrefix: "SOCKET_",
+// 		},
+// 	})
+// );
 
 import axios from "./axios.js";
 Vue.prototype.$http = axios;
--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -1,22 +1,28 @@
+import Vue from "vue";
 export default {
 	data() {
 		return {
-			connection: null,
+			socket: null,
 		};
 	},
 	mounted() {
-		this.connection = new VueSocketIO({
-			debug: true,
-			secure: true,
-			transports: ["websocket", "pulling"],
-			connection:
-				"ws://dax.autovm.ir/api/v2/ranger/public/?stream=global.tickers",
-			vuex: {
-				store,
-				actionPrefix: "SOCKET_",
-				mutationPrefix: "SOCKET_",
-			},
-		});
-		connection.connect();
+		const cookie = Vue.$cookies.get("_barong_session", { httpOnly: false });
+		Vue.$cookies.set("_barong_session", cookie, { httpOnly: false });
+		// axios.defaults.headers.common["Cookie"] = `_barong_session=${cookie}`;
+
+		console.log("mixin created", cookie);
+		this.socket = new WebSocket(
+			"ws://dax.autovm.ir/api/v2/ranger/private/?stream=deposit_address&stream=ethusd.ob-inc&stream=ethusd.trades&stream=global.tickers&stream=order&stream=trade"
+		);
+		this.socket.onopen = res => {
+			console.log("on open", res);
+		};
+	},
+	methods: {
+		onOpenConnection() {
+			this.socket.onopen = res => {
+				console.log("on open", res);
+			};
+		},
 	},
 };
