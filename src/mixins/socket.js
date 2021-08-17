019548731c01974338b7d3be369ba196353cf1c1
nimatorabi <nimatorabi@rocketmail.com>
2021-08-17 12:39:08 +0430

    add cookie

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -5,6 +5,12 @@ export default {
 		};
 	},
 	mounted() {
+		const cookie = this.$cookies.get("_barong_session", { httpOnly: false });
+		this.$cookies.set("_barong_session", cookie, { httpOnly: false });
+		axios.defaults.withCredentials = true;
+		// axios.defaults.headers.common["Cookie"] = `_barong_session=${cookie}`;
+		this.$store.commit("auth/SET_COOKIE", cookie);
+
 		console.log("mixin created");
 		this.socket = new WebSocket(
 			"ws://dax.autovm.ir/api/v2/ranger/private/?stream=deposit_address&stream=ethusd.ob-inc&stream=ethusd.trades&stream=global.tickers&stream=order&stream=trade"
