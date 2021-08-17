dd8af14e5790caf6ad5a43e516b8de1b3daf7e38
nimatorabi <nimatorabi@rocketmail.com>
2021-08-17 12:37:21 +0430

    change socket io to websocket

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -6,7 +6,9 @@ export default {
 	},
 	mounted() {
 		console.log("mixin created");
-		this.socket = new WebSocket("ws://dax.autovm.ir/api/v2/ranger/private/");
+		this.socket = new WebSocket(
+			"ws://dax.autovm.ir/api/v2/ranger/private/?stream=deposit_address&stream=ethusd.ob-inc&stream=ethusd.trades&stream=global.tickers&stream=order&stream=trade"
+		);
 		this.onOpenConnection();
 	},
 	methods: {
