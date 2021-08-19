f2e25a2da73f11a273e0395cfc653a1dc47a7dba
nimatorabi <nimatorabi@rocketmail.com>
2021-08-19 08:46:31 +0430

    set cookie in websocket

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -6,13 +6,23 @@ export default {
 		};
 	},
 	mounted() {
-		const cookie = Vue.$cookies.get("_barong_session", { httpOnly: false });
-		Vue.$cookies.set("_barong_session", cookie, { httpOnly: false });
+		const cookie = Vue.$cookies.get("_barong_session", {
+			httpOnly: false,
+		});
+		Vue.$cookies.set("_barong_session", cookie, {
+			httpOnly: false,
+		});
 		// axios.defaults.headers.common["Cookie"] = `_barong_session=${cookie}`;
-
+		console.log("cookie", cookie);
 		console.log("mixin created", cookie);
 		this.socket = new WebSocket(
-			"ws://dax.autovm.ir/api/v2/ranger/public/?stream=ethusd.ob-inc,ethusd.trades,global.tickers"
+			"ws://dax.autovm.ir/api/v2/ranger/private/?stream=deposit_address&stream=ethusd.ob-inc&stream=ethusd.trades&stream=global.tickers&stream=order&stream=trade",
+			[],
+			{
+				headers: {
+					Cookie: cookie,
+				},
+			}
 		);
 		this.socket.onopen = res => {
 			console.log("on open", res);
