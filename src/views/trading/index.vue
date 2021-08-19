8991e813896e97d5fdf427e3cf0326244b124043
nimatorabi <nimatorabi@rocketmail.com>
2021-08-19 09:42:25 +0430

    call socket in component

--- src/views/trading/index.vue
+++ src/views/trading/index.vue
@@ -37,9 +37,20 @@ export default {
 		Trades,
 		TradingXs,
 	},
-	mixins: [Socket],
+	data() {
+		return {
+			socket: null,
+		};
+	},
+	// mixins: [Socket],
 	mounted() {
 		this.getTokensMarket();
+		this.socket = new WebSocket(
+			"ws://dax.autovm.ir/api/v2/ranger/private/?stream=deposit_address&stream=ethusd.ob-inc&stream=ethusd.trades&stream=global.tickers&stream=order&stream=trade"
+		);
+		this.socket.onopen = res => {
+			console.log("on open", res);
+		};
 	},
 	methods: {
 		getTokensMarket() {
