f25aed3157e5bd0abf3b5e6e0c7d6044de50a2e2
nimatorabi <nimatorabi@rocketmail.com>
2021-08-28 10:16:02 +0430

    change sign-up design

--- src/views/auth/signUp.vue
+++ src/views/auth/signUp.vue
@@ -1,91 +1,118 @@
 <template>
-	<div class="w-11/12 lg:w-4/12 mx-auto center rounded-md p-8">
-		<card-with-header>
-			<template #header>
-				<p class="text-center font-bold text-lg">
-					{{ $t("pages.auth.signUp.title") }}
-				</p>
-			</template>
+	<div class="w-11/12 lg:w-8/12 mx-auto center rounded-md p-8">
+		<card-with-header :has-header="false">
 			<template #body>
-				<ValidationObserver
-					v-slot="{ invalid }"
-					tag="form"
-					@submit.prevent="onSubmit"
-				>
-					<ValidationProvider
-						name="E-mail"
-						rules="required|email"
-						v-slot="{ errors }"
-					>
-						<vs-input
-							v-model="user.email"
-							class="min-w-full"
-							:label="$t('pages.auth.signUp.userName')"
-							:placeholder="$t('pages.auth.signUp.userNamePlaceHolder')"
-						/>
-						<span class="text-danger text-xs">{{ errors[0] }}</span>
-					</ValidationProvider>
-					<ValidationProvider
-						name="password"
-						rules="required"
-						v-slot="{ errors }"
-					>
-						<vs-tooltip :text="$t('pages.auth.signUp.rules')" position="bottom">
+				<div class="flex flex-row justify-between items-center">
+					<div class="w-full md:w-6/12 mx-2">
+						<p class="text-center font-bold text-lg">
+							{{ $t("pages.auth.signUp.title") }}
+						</p>
+						<ValidationObserver
+							v-slot="{ invalid }"
+							tag="form"
+							@submit.prevent="onSubmit"
+						>
+							<ValidationProvider
+								name="E-mail"
+								rules="required|email"
+								v-slot="{ errors }"
+							>
+								<vs-input
+									v-model="user.email"
+									class="min-w-full"
+									:label="$t('pages.auth.signUp.userName')"
+									:placeholder="$t('pages.auth.signUp.userNamePlaceHolder')"
+								/>
+								<span class="text-danger text-xs">{{ errors[0] }}</span>
+							</ValidationProvider>
+							<ValidationProvider
+								name="password"
+								rules="required"
+								v-slot="{ errors }"
+							>
+								<vs-tooltip
+									:text="$t('pages.auth.signUp.rules')"
+									position="bottom"
+								>
+									<vs-input
+										v-model="user.password"
+										type="password"
+										class="min-w-full"
+										:label="$t('pages.auth.signUp.password')"
+										:placeholder="$t('pages.auth.signUp.passwordPlaceHodler')"
+									/>
+								</vs-tooltip>
+								<span class="text-danger text-xs">{{ errors[0] }}</span>
+								<password-meter :password="user.password" />
+							</ValidationProvider>
+							<ValidationProvider
+								name="confirm password"
+								rules="required|confirmed:password"
+								v-slot="{ errors }"
+							>
+								<vs-tooltip
+									:text="$t('pages.auth.signUp.rules')"
+									position="bottom"
+								>
+									<vs-input
+										v-model="user.reapetPassword"
+										class="min-w-full"
+										type="password"
+										:label="$t('pages.auth.signUp.repeatPassword')"
+										:placeholder="
+											$t('pages.auth.signUp.repeatPasswordPlaceHodler')
+										"
+									/>
+								</vs-tooltip>
+								<span class="text-danger text-xs">{{ errors[0] }}</span>
+								<password-meter :password="user.reapetPassword" />
+							</ValidationProvider>
 							<vs-input
-								v-model="user.password"
-								type="password"
+								v-model="user.refid"
 								class="min-w-full"
-								:label="$t('pages.auth.signUp.password')"
-								:placeholder="$t('pages.auth.signUp.passwordPlaceHodler')"
+								:label="$t('pages.auth.signUp.refId')"
+								:placeholder="$t('pages.auth.signUp.refIdPlaceHolder')"
 							/>
-						</vs-tooltip>
-						<span class="text-danger text-xs">{{ errors[0] }}</span>
-						<password-meter :password="user.password" />
-					</ValidationProvider>
-					<ValidationProvider
-						name="confirm password"
-						rules="required|confirmed:password"
-						v-slot="{ errors }"
-					>
-						<vs-tooltip :text="$t('pages.auth.signUp.rules')" position="bottom">
-							<vs-input
-								v-model="user.reapetPassword"
-								class="min-w-full"
-								type="password"
-								:label="$t('pages.auth.signUp.repeatPassword')"
-								:placeholder="$t('pages.auth.signUp.repeatPasswordPlaceHodler')"
-							/>
-						</vs-tooltip>
-						<span class="text-danger text-xs">{{ errors[0] }}</span>
-						<password-meter :password="user.reapetPassword" />
-					</ValidationProvider>
-					<vs-input
-						v-model="user.refid"
-						class="min-w-full"
-						:label="$t('pages.auth.signUp.refId')"
-						:placeholder="$t('pages.auth.signUp.refIdPlaceHolder')"
-					/>
-					<div class="flex items-center justify-start mt-3">
-						<vs-checkbox v-model="user.acceptRules" />
-						<p class="text-sm">
-							{{ $t("pages.auth.signUp.faq.start") }}
-							<strong class="text-blue-400 underline">
-								{{ $t("pages.auth.signUp.faq.middle") }}
-							</strong>
-							{{ $t("pages.auth.signUp.faq.end") }}
+							<div class="flex items-center justify-start mt-3">
+								<vs-checkbox v-model="user.acceptRules" />
+								<p class="text-sm">
+									{{ $t("pages.auth.signUp.faq.start") }}
+									<strong class="text-blue-400 underline">
+										{{ $t("pages.auth.signUp.faq.middle") }}
+									</strong>
+									{{ $t("pages.auth.signUp.faq.end") }}
+								</p>
+							</div>
+							<div>
+								<vs-button
+									:disabled="invalid || !user.acceptRules"
+									class="w-full mt-5"
+									type="gradient"
+									@click.prevent="registerAccount"
+								>
+									{{ $t("pages.auth.signUp.submit") }}
+								</vs-button>
+							</div>
+						</ValidationObserver>
+						<vs-divider class="pt-1" />
+						<p
+							@click="$router.push('/auth/forget-password')"
+							class="
+								text-sm text-gray-500 text-center
+								cursor-pointer
+								font-bold
+								hover:text-blue-700
+								transition-300
+								ease-in-out
+							"
+						>
+							{{ $t("pages.auth.login.forgetPass") }}
 						</p>
 					</div>
-					<div>
-						<vs-button
-							:disabled="invalid || !user.acceptRules"
-							class="w-full mt-5"
-							type="gradient"
-							@click.prevent="registerAccount"
-						>
-							{{ $t("pages.auth.signUp.submit") }}
-						</vs-button>
+					<div class="hidden md:flex w-5/12 mx-2">
+						<img src="../../assets/img/login.png" class="object-cover" alt="" />
 					</div>
-				</ValidationObserver>
+				</div>
 			</template>
 		</card-with-header>
 	</div>
